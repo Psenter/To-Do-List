0911bc7dc8e625468f61b9024d044f7e7f7e5a20
Parker S <111610335+Psenter@users.noreply.github.com>
2023-10-18 15:03:37 -0400

    Fixed deploying to GH pages.

---
+++ todolist/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ToDoList from './routes/ToDoList.js';
+
+function App() {
+  return (
+    <div className="body">
+      <div className ="container">
+        <ToDoList />
+      </div>
+    </div>
+);
+}
+
+export default App;
